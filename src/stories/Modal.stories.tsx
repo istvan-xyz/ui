2a89c541b034b86c9ff35146e6960dfaec2e74f6
István Antal <istvan@antal.xyz>
2022-03-05 09:38:45 +0000

    Update color and add initial modal story

---
+++ src/stories/Modal.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import '../../style.css';
+import '../../page.css';
+import Modal from '../Modal';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'ui/Modal',
+    component: Modal,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof Modal>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Modal> = args => <Modal {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+    primary: true,
+    title: 'Title',
+    children: 'Example',
+    footer: 'Footer',
+};
