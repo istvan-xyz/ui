c8f42291854a8eca19c61342cc3d4521709755fe
István Antal <istvan@antal.xyz>
2022-03-09 12:32:58 +0000

    Add UpdateNotifier

---
+++ src/UpdateNotifier.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import Button from './Button';
+import ButtonRow from './ButtonRow';
+import Modal from './Modal';
+
+const mainScriptUrl = document.getElementsByTagName('script')[0].src;
+
+const UpdateNotifier = () => {
+    const [isUpToDate, setIsUpToDate] = useState(true);
+    useEffect(() => {
+        const updateCheckInterval = setInterval(() => {
+            (async () => {
+                const response = await fetch(mainScriptUrl);
+                /* eslint-disable-next-line no-console */
+                console.log(response.status);
+                if (response.status === 404) {
+                    clearTimeout(updateCheckInterval);
+                    setIsUpToDate(false);
+                }
+            })().catch(error => {
+                throw error;
+            });
+        }, 30_000);
+
+        return () => {
+            clearTimeout(updateCheckInterval);
+        };
+    });
+
+    if (isUpToDate) {
+        return null;
+    }
+    return (
+        <Modal
+            title="New version available"
+            footer={
+                <ButtonRow>
+                    <Button
+                        primary
+                        onPress={() => {
+                            /* eslint-disable-next-line no-restricted-globals */
+                            location.reload();
+                        }}
+                    >
+                        Reload
+                    </Button>
+                </ButtonRow>
+            }
+        >
+            Please reload.
+        </Modal>
+    );
+};
+
+export default UpdateNotifier;
