58022fda4bd1ac7697e7f8477af9c06a5191b964
István Antal <istvan@antal.xyz>
2022-03-09 12:58:02 +0000

    Handle chrome-extension in UpdateNotifier

--- src/UpdateNotifier.tsx
+++ src/UpdateNotifier.tsx
@@ -3,13 +3,18 @@ import Button from './Button';
 import ButtonRow from './ButtonRow';
 import Modal from './Modal';
 
-const mainScriptUrl = document.getElementsByTagName('script')[0].src;
+const mainScriptUrl = Array.from(document.getElementsByTagName('script')).find(
+    item => !item.src.startsWith('chrome-extension://')
+)?.src;
 
 const UpdateNotifier = () => {
     const [isUpToDate, setIsUpToDate] = useState(true);
     useEffect(() => {
         const updateCheckInterval = setInterval(() => {
             (async () => {
+                if (!mainScriptUrl) {
+                    return;
+                }
                 const response = await fetch(mainScriptUrl);
                 /* eslint-disable-next-line no-console */
                 console.log(response.status);
